354835fa3a9fe1a9397ab47add1dd4b767f1966a
Richard Jones <richard@cottagelabs.com>
2015-07-31 08:59:34 +0100

    prep for development

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'YOURAPP',
+    name = 'jper-sword-out',
     version = '1.0.0',
     packages = find_packages(),
     install_requires = [
@@ -12,12 +12,9 @@ setup(
     url = 'http://cottagelabs.com/',
     author = 'Cottage Labs',
     author_email = 'us@cottagelabs.com',
-    description = 'DESCRIBE YOUR APP',
+    description = 'Consumes notifications from JPER and pushes them to repositories via swordv2',
     classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: Copyheart',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'
